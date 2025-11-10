017821523f2ca77689d52a9e0536836134293445
JuanKVargas20 <juancamilovargasperdono.com>
2025-11-09 22:58:59 -0500

    Actualización: configuración Vite, dependencias y hook useAuth

--- noticias-cms/vite.config.ts
+++ noticias-cms/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/NoticiaProyecto_2025g1_Vargas_Juan/',
+  plugins: [react()]
 })
